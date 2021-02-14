49852f49d4d5a63725534c8574560622f2e37066
Sergei <spodshivalov@scmarinetech.com>
2021-02-14 12:03:34 -0800

    Added ottopi Bluetooth service

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ PI=rpi
 
 ssh pi@${PI} 'sudo systemctl stop ottopi.service'
 ssh pi@${PI} 'sudo systemctl disable ottopi.service'
+ssh pi@${PI} 'sudo systemctl stop ottopi-bt.service'
+ssh pi@${PI} 'sudo systemctl disable ottopi-bt.service'
 
 # Build server app
 pushd web
@@ -16,9 +18,12 @@ popd
 # Copy navcomputer to RPI
 ssh pi@${PI} 'mkdir -p ottopi'
 ssh pi@${PI} 'mkdir -p ottopi/web'
-scp  -r navcomputer pi@${PI}:ottopi/
-scp  -r web/build   pi@${PI}:ottopi/web
-scp  -r ottopi.service   pi@${PI}:ottopi/
+
+scp  -r navcomputer        pi@${PI}:ottopi/
+scp  -r bt_remote          pi@${PI}:ottopi/
+scp  -r web/build          pi@${PI}:ottopi/web
+scp  -r ottopi.service     pi@${PI}:ottopi/
+scp  -r ottopi-bt.service  pi@${PI}:ottopi/
 
 # Install required Python packages
 
@@ -27,7 +32,11 @@ ssh pi@${PI} 'sudo pip3 install -r ottopi/navcomputer/requirements.txt'
 # Start service
 
 ssh pi@${PI} 'sudo cp ottopi/ottopi.service /etc/systemd/system/ottopi.service'
+ssh pi@${PI} 'sudo cp ottopi/ottopi-bt.service /etc/systemd/system/ottopi-bt.service'
 ssh pi@${PI} 'sudo systemctl enable ottopi.service'
 ssh pi@${PI} 'sudo systemctl start ottopi.service'
+ssh pi@${PI} 'sudo systemctl enable ottopi-bt.service'
+ssh pi@${PI} 'sudo systemctl start ottopi-bt.service'
 
 ssh pi@${PI} 'systemctl status ottopi'
+ssh pi@${PI} 'systemctl status ottopi-bt'
