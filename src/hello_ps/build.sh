2f7333a6c8aa673be5f79a9c46ccf7aeb31bb455
Sergei <spodshivalov@scmarinetech.com>
2021-02-15 10:14:20 -0800

    Removed old unused files

--- src/hello_ps/build.sh
+++
@@ -1,3 +0,0 @@
-gcc -o hello_ps hello_ps.c \
-    -DMODELDIR=\"`pkg-config --variable=modeldir pocketsphinx`\" \
-    `pkg-config --cflags --libs pocketsphinx sphinxbase`
--- src/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo Copying executables to the pi ...
-scp voice2nmea pi@pi:
-scp text2sound pi@pi:
-
-echo Done. Please launch manually.
