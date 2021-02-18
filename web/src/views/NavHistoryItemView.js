4ff845d978f1c5311ad82d0ed21db2215c70948a
Sergei <spodshivalov@scmarinetech.com>
2021-02-17 18:18:18 -0800

    Applied material design to the nav history

--- web/src/views/NavHistoryItemView.js
+++ web/src/views/NavHistoryItemView.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Moment from 'react-moment';
+import {TableCell, TableRow, Typography} from "@material-ui/core";
 
 function NavHistoryItemView(props) {
+
+    const is_starboard = props.twa >= 0;
+
     return (
-        <div>
-            HDG {props.hdg.toFixed(0)}
-            ( <Moment date={props.utc} format="hh:mm:ss" /> )
-        </div>
+        <TableRow>
+            <TableCell align="center"><Typography variant="h6"><Moment date={props.utc} format="hh:mm:ss" /></Typography></TableCell>
+            <TableCell align="center">
+                <Typography variant="h6">{ is_starboard ? props.hdg.toFixed(0) : ''}</Typography>
+            </TableCell>
+            <TableCell align="center"><Typography variant="h6">{ !is_starboard ? props.hdg.toFixed(0) : ''}</Typography></TableCell>
+        </TableRow>
     );
 }
 export default NavHistoryItemView;
--- web/src/views/NavHistoryListView.js
+++ web/src/views/NavHistoryListView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavHistoryItemView from "./NavHistoryItemView";
+import {Paper, Table, TableBody, TableContainer} from "@material-ui/core";
 
 function NavHistoryListView(props){
 
@@ -10,32 +11,23 @@ function NavHistoryListView(props){
 
             if( props.ok ){
                 let i;
-                let prevTwa = 0;
-                let courseChanged = false;
                 const last_item = props.items.length - 1;
                 for (i = last_item; i >= 0; i--) {
-                    let twa = props.items[i].twa;
-                    if ( i === last_item ){
-                        courseChanged = true;
-                    }else{
-                        courseChanged = Math.abs(twa - prevTwa) > 45;
-                    }
-                    if ( courseChanged) {
-                        prevTwa =  twa;
-                        const tack = twa >= 0 ? 'starboard' : 'port';
-                        twa = Math.abs(twa)
-                        const upDown = twa <= 90 ? 'up wind' : 'downwind';
-                        history_items.push( <div> {upDown} {tack} </div>);
-                    }
-                    history_items.push( <NavHistoryItemView {...props.items[i]} key={i} /> );
+                    history_items.push(
+                        <NavHistoryItemView {...props.items[i]} key={i} />
+                        );
                 }
             }else{
                 history_items = 'Failed to fetch nav history';
             }
             return (
-                <div>
+                <TableContainer component={Paper}>
+                    <Table size={'small'} >
+                        <TableBody>
                     {history_items}
-                </div>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             )
         }
 }
