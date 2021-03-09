0b60f92059b6293f0efc0b2e555187e489935ecc
sergei <spodshivalov@scmarinetech.com>
2021-03-08 16:51:18 -0800

    Renamed files to conf menu item

--- web/src/views/MenuButtonsView.js
+++ web/src/views/MenuButtonsView.js
@@ -4,9 +4,8 @@ import {BottomNavigation, BottomNavigationAction} from "@material-ui/core";
 import ExploreIcon from '@material-ui/icons/Explore';
 import ShowChartIcon from '@material-ui/icons/ShowChart';
 import BrightnessAutoIcon from '@material-ui/icons/BrightnessAuto';
-import SdCardIcon from '@material-ui/icons/SdCard';
 import TimerIcon from '@material-ui/icons/Timer';
-
+import TuneIcon from '@material-ui/icons/Tune';
 function MenuButtonsView(props) {
 
     return (
@@ -19,9 +18,9 @@ function MenuButtonsView(props) {
             >
                 <BottomNavigationAction value={screens.NAVIGATION} label="Navigation" icon={<ExploreIcon />} />
                 <BottomNavigationAction value={screens.ROUTES} label="Routes" icon={<ShowChartIcon />} />
-                <BottomNavigationAction value={screens.FILE_MANAGER} label="Files" icon={<SdCardIcon />} />
-                <BottomNavigationAction value={screens.AUTOPILOT} label="Autopilot" icon={<BrightnessAutoIcon />} />
                 <BottomNavigationAction value={screens.RACE_TIMER} label="Timer" icon={<TimerIcon />} />
+                <BottomNavigationAction value={screens.AUTOPILOT} label="Autopilot" icon={<BrightnessAutoIcon />} />
+                <BottomNavigationAction value={screens.FILE_MANAGER} label="Conf." icon={<TuneIcon />} />
             </BottomNavigation>
     );
 
