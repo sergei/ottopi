9b43c1d47683cac4598420f7a6c4832b8da64e74
Sergei <spodshivalov@scmarinetech.com>
2021-03-23 15:34:43 -0700

    Able to send IMU to SignalK now

--- imu/data_sender.py
+++ imu/data_sender.py
@@ -1,20 +1,84 @@
 import json
 import math
+import os
+import time
+import uuid
 
+import requests
 import websocket
 
+SIGNAL_K_HOST = 'http://localhost:3000'
+SIGNAL_K_ACCESS_URL = SIGNAL_K_HOST + "/signalk/v1/access/requests"
 LOCAL_SIGNAL_K_URL = "ws://localhost:3000/signalk/v1/stream"
 LOCAL_NAVCOMPUTER_URL = "ws://localhost:5555/signalk/v1/stream"
 
 
 class DataSender:
-    def __init__(self, url=LOCAL_SIGNAL_K_URL):
+    def __init__(self, url, data_dir):
         self.url = url
         self.ws = websocket.WebSocket()
         self.is_connected = False
+        self.config_file = os.path.expanduser(data_dir + os.sep + 'signalk-auth.json')
+        self.config = {
+            'client_id': None,
+            'token': None
+        }
+        try:
+            with open(self.config_file, 'r') as f:
+                self.config = json.load(f)
+        except Exception as e:
+            print(e)
+        if self.config['client_id'] is None:
+            self.config['client_id'] = str(uuid.uuid4())
+            self.store_cfg()
+
+    def store_cfg(self):
+        with open(self.config_file, 'w') as f:
+            json.dump(self.config, f)
+
+    def request_access(self):
+        data = {"clientId": self.config['client_id'],
+                "description": "OttoPi IMU"
+                }
+        r = requests.post(SIGNAL_K_ACCESS_URL, json=data)
+        print(r)
+        resp = json.loads(r.text)
+        if 'href' in resp:
+            self.store_cfg()
+            print('Waiting for approval')
+            while True:
+                time.sleep(5)
+                r = requests.get(SIGNAL_K_HOST + resp['href'])
+                print(r)
+                resp = json.loads(r.text)
+                if resp['statusCode'] == 400:  # Already approved
+                    break
+                if resp['statusCode'] == 200:
+                    if resp['accessRequest']['permission'] == 'APPROVED':
+                        self.config['token'] = resp['accessRequest']['token']
+                        self.store_cfg()
+                        break
 
     def connect(self):
-        self.ws.connect(self.url)
+        while self.config['token'] is None:
+            try:
+                self.request_access()
+            except Exception as e:
+                print(e)
+
+        try:
+            self.ws.connect(self.url, header=[
+                'Authorization: Bearer {}'.format(self.config['token'])
+            ])
+        except websocket.WebSocketBadStatusException as e:
+            if e.status_code == 401:
+                self.config['token'] = None
+            print('WebSocketBadStatusException ' + str(e))
+            raise e
+
+        # Read hello
+        r = self.ws.recv()
+        print('Hello:' + r)
 
     def close(self):
         self.ws.close()
@@ -25,7 +89,7 @@ class DataSender:
                 self.connect()
                 self.is_connected = True
             except Exception as e:
-                print(e)
+                print('Connect error' + str(e))
                 return
 
         data = {
@@ -54,10 +118,12 @@ class DataSender:
             self.ws.send(json.dumps(data))
         except Exception as e:
             self.is_connected = False
-            print(e)
+            print('Send error' + str(e))
+
+    def authorize(self):
+        pass
 
 
 if __name__ == '__main__':
-    # data_sender = DataSender()
-    data_sender = DataSender(LOCAL_NAVCOMPUTER_URL)
+    data_sender = DataSender(LOCAL_NAVCOMPUTER_URL, '.')
     data_sender.send(270, 10, -10)
--- imu/main.py
+++ imu/main.py
@@ -1,14 +1,19 @@
+import argparse
+import os
 import time
 
+from data_sender import DataSender, LOCAL_SIGNAL_K_URL
 from fusion import Fusion
 from raw_sensors import RawSensors
 from time_diff import time_diff
 
 
-def main(csv_log_name=None):
+def main(args):
     raw = RawSensors()
     fusion = Fusion(timediff=time_diff)
+    data_sender = DataSender(LOCAL_SIGNAL_K_URL, args.data_dir)
 
+    csv_log_name = os.path.expanduser(args.log_dir + os.sep + 'sensors.csv') if args.log_dir is not None else None
     csv_file = open(csv_log_name, 'wt') if csv_log_name is not None else None
     if csv_file is not None:
         csv_file.write('t,ax,ay,az,gx,gy,gz,mx,my,mz,heading,pitch,roll\n')
@@ -16,7 +21,8 @@ def main(csv_log_name=None):
         try:
             accel, gyro, mag, t = raw.get_raw_data()
             fusion.update(accel, gyro, mag, t)
-            print('heading {:.3f}, pitch {:.3f}, roll {:.3f}'.format(fusion.heading, fusion.pitch, fusion.roll))
+            # print('heading {:.3f}, pitch {:.3f}, roll {:.3f}'.format(fusion.heading, fusion.pitch, fusion.roll))
+            data_sender.send(fusion.heading, fusion.pitch, fusion.roll)
             if csv_file is not None:
                 csv_file.write('{},{},{},{},{},{},{},{},{},{},{},{},{}\n'.format(t,
                                                                                  accel[0], accel[1], accel[2],
@@ -27,8 +33,12 @@ def main(csv_log_name=None):
                                                                                  ))
         except OSError as e:
             print('Failed to read sensors {}'.format(e))
-        time.sleep(0.01)
+        time.sleep(0.1)
 
 
 if __name__ == '__main__':
-    main('sensors.csv')
+    parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
+    parser.add_argument("--log-dir", help="Directory to store logs",  required=False)
+    parser.add_argument("--data-dir", help="Directory to keep config data",  required=True)
+
+    main(parser.parse_args())
