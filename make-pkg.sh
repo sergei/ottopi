e5af8fa789089a7390843b7b43cf1c32f7f13465
Sergei <spodshivalov@scmarinetech.com>
2021-03-23 13:54:30 -0700

    Modified service to listen to SignalK TCP server instead of reading straight from port

--- make-pkg.sh
+++ make-pkg.sh
@@ -6,5 +6,5 @@ yarn build
 popd || exit
 
 tar cvzf update/otto-pi-update.tgz  --exclude='navcomputer/__pycache__/' --exclude='bt_remote/__pycache__/' \
-     navcomputer/ bt_remote/ web/build imu/ update/run-update.sh
+     *.service navcomputer/ bt_remote/ web/build imu/ update/run-update.sh
 
